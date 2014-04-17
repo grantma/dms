e1d3ac5b94570036aaba6c41e39ca7eb6f6cbf5b
Matthew Grant <grantma@en-gedi.internal.anathoth.net>
2014-04-17 15:19:38 +1200

    Update version number to 1.0.1g

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 from distutils.core import setup
 
 setup(name='DMS',
-    version='1.0.1f',
+    version='1.0.1g',
     description='DMS Master System',
     author='Matthew Grant',
     author_email='matt@mattgrant.net.nz',
